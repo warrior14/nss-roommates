8889c0ce7284a93be8547895dc467af64a48fa7c
warrior14 <ironaddicts14@gmail.com>
2021-09-10 14:16:56 -0500

    added a RoomRepository.cs, BaseRepository and added methods in RoomRepository.cs

---
+++ Repositories/RoomRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
+
+
+
+
+namespace Roommates.Repositories
+{
+    /// <summary>
+    ///  This class is responsible for interacting with Room data.
+    ///  It inherits from the BaseRepository class so that it can use the BaseRepository's Connection property
+    /// </summary>
+    public class RoomRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new RoomRepository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public RoomRepository(string connectionString) : base(connectionString) { }
+
+        // ...We'll add some methods shortly...
+    }
+}
